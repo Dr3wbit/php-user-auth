aa56a5ab64b745594b6ba5a30166d9ee8b78ae30
Dr3wbit <eightbitandrew@gmail.com>
2018-05-09 14:14:52 -0700

    initial setup completed

---
+++ user_data.php
@@ -0,0 +1,19 @@
+<?php
+
+$user_data = [
+    'discostu' => [
+        'name' => 'Steven',
+        'password' => 'asdf1234',
+        'user_level' => 2
+    ],
+    'sideshowbob' => [
+        'name' => 'Robert',
+        'password' => 'asdf1234',
+        'user_level' => 1
+    ],
+    'lsimpson' => [
+        'name' => 'Lisa',
+        'password' => 'asdf1234',
+        'user_level' => 3
+    ]
+];
